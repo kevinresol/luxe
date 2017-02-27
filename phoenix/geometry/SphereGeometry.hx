7e7f0ab66afa9973abc80d2034c402745c7fdea4
Kevin Leung <kevinresol@gmail.com>
2017-02-27 17:47:46 +0800

    SphereGeometry; add uv

--- phoenix/geometry/SphereGeometry.hx
+++ phoenix/geometry/SphereGeometry.hx
@@ -1,5 +1,6 @@
 package phoenix.geometry;
 
+import luxe.*;
 import luxe.Log.*;
 import luxe.options.GeometryOptions;
 import phoenix.*;
@@ -10,7 +11,13 @@ class SphereGeometry extends Geometry {
     public var flipx(default, set):Bool = false;
     public var flipy(default, set):Bool = false;
     
+    var uvs:Array<TextureCoord>;
     var verts:Array<Vertex>;
+    
+    var _uv_x:Float = 0;
+    var _uv_y:Float = 0;
+    var _uv_w:Float = 1;
+    var _uv_h:Float = 1;
 
     public function new( _options:SphereGeometryOptions ) {
         
@@ -19,6 +26,12 @@ class SphereGeometry extends Geometry {
         super(_options);
         
         generate_vertices(_options);
+        
+        if(_options.uv != null) {
+            uv(_options.uv);
+        } else {
+            uv_space(0, 0, 1, 1);
+        }
         
         if(_options.flipx != null) flipx = _options.flipx;
         if(_options.flipy != null) flipy = _options.flipy;
@@ -39,6 +52,7 @@ class SphereGeometry extends Geometry {
         var rot = new Matrix();
         var vec = new Vector();
         
+        uvs = [];
         verts = [];
 
         for (z in 0...z_steps + 1) {
@@ -57,6 +71,7 @@ class SphereGeometry extends Geometry {
                 
                 var vertex = new Vertex(vec.clone(), _options.color, vec.normalize().clone());
                 vertex.uv.uv0.set_uv(normalized_y, 1 - normalized_z);
+                uvs.push(new TextureCoord(normalized_y, 1 - normalized_z));
                 verts.push(vertex);
                 
             }
@@ -84,19 +99,48 @@ class SphereGeometry extends Geometry {
         }
     }
     
+    public function uv_space(x:Float, y:Float, w:Float, h:Float) {
+        
+        _uv_x = x;
+        _uv_y = y;
+        _uv_w = w;
+        _uv_h = h;
+        
+        for(i in 0...verts.length) {
+            verts[i].uv.uv0.u = x + w * (flipx ? 1 - uvs[i].u : uvs[i].u);
+            verts[i].uv.uv0.v = y + h * (flipy ? 1 - uvs[i].v : uvs[i].v);
+        }
+    }
+    
+    public inline function uv( _rect:luxe.Rectangle ) {
+
+        assertnull(texture, "SphereGeometry; Calling UV on a geometry with null texture.");
+
+        var tlx = _rect.x/texture.width_actual;
+        var tly = _rect.y/texture.height_actual;
+        var szx = _rect.w/texture.width_actual;
+        var szy = _rect.h/texture.height_actual;
+
+        uv_space( tlx, tly, szx, szy );
+
+    } //uv
+    
+    
     function set_flipx(v:Bool) {
         
         if(flipx == v) return v;
-        for(v in verts) v.uv.uv0.u = 1 - v.uv.uv0.u;
-        return flipx = v;
+        flipx = v;
+        uv_space(_uv_x, _uv_y, _uv_w, _uv_h);
+        return v;
         
     }
     
     function set_flipy(v:Bool) {
         
         if(flipy == v) return v;
-        for(v in verts) v.uv.uv0.v = 1 - v.uv.uv0.v;
-        return flipy = v;
+        flipy = v;
+        uv_space(_uv_x, _uv_y, _uv_w, _uv_h);
+        return v;
         
     }
 }
@@ -113,5 +157,7 @@ typedef SphereGeometryOptions = {
     @:optional var flipx : Bool;
     
     @:optional var flipy : Bool;
+    
+    @:optional var uv : Rectangle;
     
 }
